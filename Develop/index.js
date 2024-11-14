e084ed327bb23d1864d81409363a7e8dabc70cac
Kimberly Nelson <129911891+gypze@users.noreply.github.com>
2024-11-13 22:00:58 -0600

    I added inquirer, as well as all questions needed, I also updated the generateMarkdown.js, and deleted one of the .gitignore files as it wasn't neccessary

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a description of your project.'
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Please provide installation instructions for your project.'
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Please provide usage information for your project.'
+},
+{
+    type: 'input',
+    name: 'contribution',
+    message: 'Please provide contribution guidelines for your project.'
+},
+{
+    type: 'input',
+    name: 'test',
+    message: 'Please provide test instructions for your project.'
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'Please select a license for your project.',
+    choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None']
+},
+{
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?'
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.createPrompt(questions).then((answers) => {
+        const markdown = generateMarkdown(answers);
+        writeToFile('README.md', markdown);
+    });
+}
 
 // Function call to initialize app
 init();
